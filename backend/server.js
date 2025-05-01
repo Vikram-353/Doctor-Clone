24ee342dce9f491ecd92895ca61d9c6297f490bf
Vikram <vikramranjan71122@gmail.com>
2025-05-01 11:39:11 +0530

    port

--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,24 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+
 const doctorRoutes = require("./routes/doctorRoutes");
 const connectDB = require("./config/db");
 
 const app = express();
+const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
 connectDB();
 
 app.use("/api", doctorRoutes);
-const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("API WORKING GREAT");
 });
 
 app.listen(port, () => {
-  console.log("Server started on port 5000");
+  console.log(`Server started on port ${port}`);
 });
