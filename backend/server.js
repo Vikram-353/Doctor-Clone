522a6f0d0898b9711a05e59b144deb9b3ca5a8a8
Vikram <vikramranjan71122@gmail.com>
2025-05-01 11:31:45 +0530

    port

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ app.use(express.json());
 connectDB();
 
 app.use("/api", doctorRoutes);
+const port = process.env.PORT || 5000;
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("Server started on port 5000");
 });
