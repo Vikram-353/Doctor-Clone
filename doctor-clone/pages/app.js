1a8f3c8c1ddfce9b35c38cf3a6e9156a30e9c3d5
Vikram <vikramranjan71122@gmail.com>
2025-05-01 12:13:55 +0530

    port

--- doctor-clone/pages/app.js
+++
@@ -1,7 +0,0 @@
-// pages/_app.js
-// import "../../styles/globals.css";
-// import "../app/globals.css";
-
-// export default function App({ Component, pageProps }) {
-//   return <Component {...pageProps} />;
-// }
